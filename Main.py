3e16cd82c2be65f57f6c1c695be0e95069696ef5
Quentin LEPRAT <q_leprat@yahoo.fr>
2016-07-10 19:14:42 +0200

    Some Refactor

--- Main.py
+++ Main.py
@@ -4,38 +4,38 @@ import Books
 window = Tk()
 window['bg']='white'
 
-FrameMenu = Frame(window, bg="gray", width=200, height=600)
-FrameMenu.pack(side=LEFT, fill=Y)
+frameMenu = Frame(window, bg="gray", width=200, height=600)
+frameMenu.pack(side=LEFT, fill=Y)
 
-FrameContent = Frame(window, bg="white", width=900, height=600)
-FrameContent.pack(side=RIGHT, fill=X)
+frameContent = Frame(window, bg="white", width=900, height=600)
+frameContent.pack(side=RIGHT, fill=X)
 
-FrameLabelMenu = Frame(FrameMenu, bg="darkgrey", highlightcolor="grey", width=200, height=100)
-FrameLabelMenu.pack(side=TOP)
-LabelTitle = Label(FrameLabelMenu, text="BookRead", bg="darkgray").pack(padx=80, pady=20)
+frameLabelMenu = Frame(frameMenu, bg="darkgrey", highlightcolor="grey", width=200, height=100)
+frameLabelMenu.pack(side=TOP)
+labelTitle = Label(frameLabelMenu, text="BookRead", bg="darkgray").pack(padx=80, pady=20)
 
 
 #Show Single Book Details
-#singleBookShow = Books.Books("TEST", "TEST", "TEST", FrameContent)
+#singleBookShow = Books.Books("TEST", "TEST", "TEST", frameContent)
 #singleBookShow.single_book()
 
 # Show All Books
 
-#allBooksShow = Books.Books("Test", "Toto", "Tata", FrameContent)
+#allBooksShow = Books.Books("Test", "Toto", "Tata", frameContent)
 #allBooksShow.show_book()
 
 # Content Menu
 
-FrameBooks = Frame(FrameMenu, bg="gray")
-FrameBooks.pack()
-LabelBooks = Label(FrameBooks, text="Books", highlightcolor="darkgrey", bg="grey").pack(padx=10, pady=20)
+frameBooks = Frame(frameMenu, bg="gray")
+frameBooks.pack()
+labelBooks = Label(frameBooks, text="Books", highlightcolor="darkgrey", bg="grey").pack(padx=10, pady=20)
 
-FrameRead = Frame(FrameMenu, bg="gray")
-FrameRead.pack()
-LabelRead = Label(FrameRead, text="Read", highlightcolor="darkgrey", bg="grey").pack(padx=10, pady=20)
+frameRead = Frame(frameMenu, bg="gray")
+frameRead.pack()
+labelRead = Label(frameRead, text="Read", highlightcolor="darkgrey", bg="grey").pack(padx=10, pady=20)
 
-FrameToRead = Frame(FrameMenu, bg="gray")
-FrameToRead.pack()
-LabelToRead = Label(FrameToRead, text="To Read", highlightcolor="darkgrey", bg="grey").pack(padx=10, pady=20)
+frameToRead = Frame(frameMenu, bg="gray")
+frameToRead.pack()
+labelToRead = Label(frameToRead, text="To Read", highlightcolor="darkgrey", bg="grey").pack(padx=10, pady=20)
 
 window.mainloop()
